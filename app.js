d1e15a7dbad5767ff2c3685a9b41ec94cbeab1fd
Fabiano Ugolini Ladeira <107953287+xUgolini@users.noreply.github.com>
2023-01-05 11:21:50 -0300

    console log indicando modificacoes do paulo

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("O paulo fez esse commit sem você ver na aula")
